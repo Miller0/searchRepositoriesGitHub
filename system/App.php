de6d71b045f942134368a27a0dfcb1f953a61228
Miller0 <mr.milller2017.11@gmail.com>
2020-10-04 01:18:37 +0300

    *

--- system/App.php
+++ system/App.php
@@ -19,7 +19,7 @@ class App
         $pathParts = explode('/', $path);
 
        //Перенаправляем на модуль
-        if (isset($pathParts[1]) && $pathParts[1]) == 'api')
+        if (isset($pathParts[1]) && $pathParts[1] == 'api')
         {
             $part = 2;
             $namespace = 'api';
